21a99b953ae2958861d42b3ec3ac82fba8674c91
Sachin Tedwa <tedwasachin123@gmail.com>
2022-07-12 05:34:22 +0530

    all done except css

--- pages/vending_machine.js
+++ pages/vending_machine.js
@@ -1,19 +1,77 @@
 import Head from 'next/head'
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 import Web3 from 'web3'
 import 'bulma/css/bulma.css';
 import style from '../styles/VendingMachine.module.css';
+import VendingMachinecontact from '../build/contracts/vending.js';
 
 
 const VendingMachine=()=>{
-    let web3
-    const [error,seterror] = useState("")
+    
+    const [error,seterror] = useState("");
+    const [success, setsuccess] = useState("");
+    const [inventory,getinventory] = useState("");
+    const [mydonutcount,getmydonutcount] = useState("");
+    const [buydonut, setbuydonut] = useState("");
+    const [web3,setweb3] = useState(null);
+    const [address,setAddress] = useState(null);
+    const [vmcontract, setvmcontract] = useState(null);
+
+    useEffect(()=>{
+        if(address) connectWalletHandler()
+        if(vmcontract) getinventoryhandler() //useeffect will run the function once page get fully load
+        if(vmcontract && address) getmydonutcounthandler()
+    },[vmcontract, address])
+
+    const getinventoryhandler = async()=>{
+        const inventory = await vmcontract.methods.getvendingmachinebalance().call();
+        getinventory(inventory)
+    }
+
+    const getmydonutcounthandler = async()=>{
+        
+        const count = await vmcontract.methods.donutbalance(address).call();
+        getmydonutcount(count)
+    }
+    const  updatedonutquantity = async(event)=>{
+        setbuydonut(event.target.value)
+        
+    }
+    const buydonuthandler=async()=> {
+        try{
+            await vmcontract.methods.purchase(buydonut).send({
+                from:address,
+                value: web3.utils.toWei('2','ether')*buydonut
+            })
+            setsuccess('donut purchase !!')
+            seterror('')
+            getinventoryhandler()
+            getmydonutcounthandler()
+            setbuydonut("")
+        } catch(err){
+            seterror(err.message)
+        }
+        
+        
+     }
+
     //window.ethereum
     const connectWalletHandler = async() =>{
+        //check metamask is available 
         if(typeof window !== "undefined" && typeof window.ethereum !=="undefined" ){
            try {
-           await window.ethereum.request({method:"eth_requestAccounts"})
-           web3 = new Web3(window.ethereum)}
+           await window.ethereum.request({method:"eth_requestAccounts"}) //request wallet connect
+
+           web3 = new Web3(window.ethereum) //set web3 instance
+
+           setweb3(web3) 
+           const accounts = await web3.eth.getAccounts();
+           
+           setAddress(accounts[0]) //get list of accounts
+
+           const vm = VendingMachinecontact(web3)//create local contract copy
+           setvmcontract(vm)
+           }
            catch(err){
             seterror(err.message);
            }
@@ -42,7 +100,24 @@ const VendingMachine=()=>{
 
         <section>
             <div className='container'>
-                <p>placeholder text</p>
+                <h2>Vending Machine Inventory: {inventory}</h2>
+            </div>
+        </section>
+        <section>
+            <div className='container'>
+                <h2>My Donut: {mydonutcount}</h2>
+            </div>
+        </section>
+        <section className='mt-5'>
+            <div className='container'>
+                <div className='field'>
+                    <label className='label'>Buy Donut</label>
+                    <div className='control'>
+                        <input value={buydonut} onChange={updatedonutquantity} className='input' type="number" placeholder='enter amount'></input>
+                    </div>
+                    <button onClick={buydonuthandler} className='button is-primary mt-2'>Buy </button>
+
+                </div>
             </div>
         </section>
         <section>
@@ -50,6 +125,12 @@ const VendingMachine=()=>{
                 <p>{error}</p>
             </div>
         </section>
+        <section>
+            <div className='container has-text-success'>
+                <p>{success}</p>
+            </div>
+        </section>
+        
         </div>
     
     )
